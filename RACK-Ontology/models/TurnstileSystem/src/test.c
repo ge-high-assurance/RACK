c3be1f316971e1284eea1a1f6540dd848e6e92be
Langston Barrett <langston.barrett@gmail.com>
2020-06-23 14:49:45 -0700

    TurnstileSystem: Add CounterApplicationUnitTesting

--- RACK-Ontology/models/TurnstileSystem/src/test.c
+++ RACK-Ontology/models/TurnstileSystem/src/test.c
@@ -1,31 +1,31 @@
 #include "test.h"
 #include <stdbool.h>
 
-int tc_1_1(void) {
-  printf("TC-1-1: PASS\n");
+int utc_1_1(void) {
+  printf("UTC-1-1: PASS\n");
   return false;
 }
 
-int tc_1_2(void) {
-  printf("TC-1-2: PASS\n");
+int utc_1_2(void) {
+  printf("UTC-1-2: PASS\n");
   return false;
 }
 
-int tc_1_3(void) {
-  printf("TC-1-3: PASS\n");
+int utc_1_3(void) {
+  printf("UTC-1-3: PASS\n");
   return false;
 }
 
-int tc_1_4(void) {
-  printf("TC-1-4: FAIL\n");
+int utc_1_4(void) {
+  printf("UTC-1-4: FAIL\n");
   return true;
 }
 
 int main(void) {
   bool failed = false;
-  failed |= tc_1_1();
-  failed |= tc_1_2();
-  failed |= tc_1_3();
-  failed |= tc_1_4();
+  failed |= utc_1_1();
+  failed |= utc_1_2();
+  failed |= utc_1_3();
+  failed |= utc_1_4();
   return failed;
 }

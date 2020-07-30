c66559ad1547822db97690c512c141769e142f36
Langston Barrett <langston.barrett@gmail.com>
2020-07-30 16:29:17 -0700

    Make rack.py into a real Python package, rename to RACK CLI
    Also, use a Pytest fixture to ensure RACK-in-a-box is up and at the correct
    version before running tests.

---
+++ RACK-Ontology/cli/tests/conftest.py
@@ -0,0 +1,19 @@
+"""Pytest fixtures and configuration"""
+
+import pytest
+
+from time import sleep
+
+from semtk3 import check_services, set_host
+
+@pytest.fixture(scope="session")
+def rack_in_a_box(docker_ip, docker_services) -> str:
+    """Ensure that RACK-in-a-box is up and responsive."""
+    url = "http://{}".format(docker_ip)
+    set_host(url)
+    # TODO(lb): For some reason, check_services always returns false.
+    # docker_services.wait_until_responsive(
+    #     timeout=240.0, pause=0.1, check=lambda: check_services()
+    # )
+    sleep(120)
+    return url

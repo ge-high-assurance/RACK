47cd82e8013217bacadb63135dd48778d2fe65c4
Eric Mertens <emertens@galois.com>
2020-08-17 17:08:10 -0700

    Update setup-rack.sh

--- RACK-Ontology/cli/setup-rack.sh
+++ RACK-Ontology/cli/setup-rack.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-set -e
+set -eu
 
 if ! command -v rack > /dev/null
 then
-        echo "rack cli tool not found in PATH"
-        exit 1
+    echo "rack cli tool not found in PATH"
+    exit 1
 fi
 
-rack model      import ../OwlModels/import.yaml
+rack model import ../OwlModels/import.yaml
 rack nodegroups import ../../nodegroups/ingestion
 rack nodegroups import ../../nodegroups/queries
-rack data       import --clear ../models/TurnstileSystem/Data/import.yaml
+rack data import --clear ../models/TurnstileSystem/Data/import.yaml

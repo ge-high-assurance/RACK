a32fcce23c4f021e066c4bff4acf8df362a85c96
Kevin Quick <kquick@galois.com>
2020-08-22 21:09:57 -0700

    Add databin with gcc and tar commands.
    The databin directory should be prefixed onto the PATH; these are
    wrappers that will invoke the standard commands but also collect
    information for RACK about the software structures.

---
+++ RACK-Ontology/databin/rackfuncs.sh
@@ -0,0 +1,18 @@
+find_in_path_remainder() { # $1 = name of executable
+    find_in_path_excluding "$1" $(dirname $0) $(dirname $0)/
+}
+
+find_in_path_excluding() { # $1 = name of executable, $2.. = exclusion paths
+    exe=$1; shift 1
+    (PATH=$(echo $(IFS=:
+                   for P in $PATH; do
+                       for excl; do
+                           if [[ "$excl" == "$P" ]] ; then
+                               continue 2
+                           fi
+                       done
+                       echo $P
+                   done
+                ) | tr ' ' :) type -p $exe)
+}
+

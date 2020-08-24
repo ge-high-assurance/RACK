d5f10dbf31da5b280de1fcf6a7d7b07c06517793
Kevin Quick <kquick@galois.com>
2020-08-24 12:26:17 -0700

    Add checks for classes not descended from PROV-S#THING.

--- RACK-Ontology/bin/check_rack.pl
+++ RACK-Ontology/bin/check_rack.pl
@@ -3,9 +3,12 @@
 
 check_rack :-
     findall(C, check_missing_notes(C), CS),
+    findall(NPC, check_not_prov_s(NPC), NPCS),
     length(CS, CSLen),
-    ((CSLen > 0, !,
-      print_message(warning, num_classes_missing_note(CSLen))) ; true).
+    length(NPCS, NPCSLen),
+    warn_if_nonzero("missing a Note/Description", CSLen),
+    warn_if_nonzero("not a subclass of PROV-S#THING", NPCSLen),
+    true.
 
 check_missing_notes(Class) :-
     rdf(Class, rdf:type, owl:'Class'),
@@ -13,7 +16,21 @@ check_missing_notes(Class) :-
     rack_ontology_node(Class, _, _),
     print_message(warning, class_missing_note(Class)).
 
+check_not_prov_s(Class) :-
+    rdf(Class, rdf:type, owl:'Class'),
+    rack_ontology_node(Class, _, _),
+    rack_ref('PROV-S#THING', Thing),
+    \+ rdf_reachable(Class, rdfs:subClassOf, Thing),
+    print_message(warning, not_prov_s_thing_class(Class)).
+
+warn_if_nonzero(What, Count) :-
+    Count > 0, !,
+    print_message(warning, num_classes(What, Count)).
+warn_if_nonzero(_, _).
+
 prolog:message(class_missing_note(Class)) -->
     [ 'No Note/Description for class ~w'-[Class] ].
-prolog:message(num_classes_missing_note(CSLen)) -->
-    [ 'There are ~d RACK definitions missing a Note/Description.'-[CSLen] ].
+prolog:message(not_prov_s_thing_class(Class)) -->
+    [ 'Not a subclass of PROV-S#THING: ~w'-[Class] ].
+prolog:message(num_classes(What, Count)) -->
+    [ 'There are ~:d RACK definitions ~w.'-[Count, What] ].

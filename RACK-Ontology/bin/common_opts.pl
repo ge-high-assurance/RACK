43a0a7b11f6fa7eccda573d388056557afbb881e
Valentin Robert <valentin.robert.42@gmail.com>
2020-09-01 13:54:59 +0200

    prolog: improve help message

--- RACK-Ontology/bin/common_opts.pl
+++ RACK-Ontology/bin/common_opts.pl
@@ -2,42 +2,53 @@
 
 :- use_module('rack_model').
 
-parse_args(ExtraArgs, Opts, PosArgs) :-
+opts_spec(Spec) :-
     working_directory(Cwd, Cwd),
     atom_concat(Cwd, 'OwlModels/', OwlDir),
     atom_concat(Cwd, 'models/TurnstileSystem/src', DataDir),
     atom_concat(Cwd, 'databin/databin.rack', RFile),
-    append([ [opt(verbose), type(boolean), default(false),
-              shortflags([v]), longflags(['verbose']),
-              help('Enable verbose output')],
+    Spec =
+    [ [opt(verbose), type(boolean), default(false),
+       shortflags([v]), longflags(['verbose']),
+       help('Enable verbose output')],
 
-             [opt(declare), type(boolean), default(false),
-              shortflags(['D']), longflags(['declare']),
-              help('Declare generated RDF triples to stdout')],
+      [opt(declare), type(boolean), default(false),
+       shortflags(['D']), longflags(['declare']),
+       help('Declare generated RDF triples to stdout')],
 
-             [opt(ontology_dir), meta('DIR_OR_URL'), type(atom),
-              shortflags([o]), longflags(['ontology', 'model']),
-              default(OwlDir),
-              help('Where to load ontology from')],
+      [opt(ontology_dir), meta('DIR_OR_URL'), type(atom),
+       shortflags([o]), longflags(['ontology', 'model']),
+       default(OwlDir),
+       help('Where to load ontology from')],
 
-             [opt(recognizers), meta('FILE'), type(atom),
-              shortflags([r]), longflags(['recognizer', 'recognizers']),
-              default(RFile),
-              help('File containing data recognizers to use')],
+      [opt(recognizers), meta('FILE'), type(atom),
+       shortflags([r]), longflags(['recognizer', 'recognizers']),
+       default(RFile),
+       help('File containing data recognizers to use')],
 
-             [opt(data_dir), meta('DIR'), type(atom),
-              shortflags([d]), longflags(['data']),
-              default(DataDir),
-              help('Where to load data from')],
+      [opt(data_dir), meta('DIR'), type(atom),
+       shortflags([d]), longflags(['data']),
+       default(DataDir),
+       help('Where to load data from')],
 
-             [opt(data_namespace), meta('NS'), type(atom),
-              shortflags([n]), longflags(['namespace']),
-              default('http://testdata.org/'),
-              help('Namespace to load data into')]
-           ], ExtraArgs, OptSpec),
+      [opt(data_namespace), meta('NS'), type(atom),
+       shortflags([n]), longflags(['namespace']),
+       default('http://testdata.org/'),
+       help('Namespace to load data into')],
+
+      [opt(help), type(boolean), default(false),
+       shortflags([h]), longflags(['help']),
+       help('Display this help')]
+
+    ].
+
+parse_args(ExtraArgs, Opts, PosArgs) :-
+    opts_spec(OptsSpec),
+    append(OptsSpec, ExtraArgs, OptSpec),
     opt_arguments(OptSpec, Opts, PosArgs),
     % write('Opts: '), write(Opts), nl,
     % write('PosArgs: '), write(PosArgs), nl,
+    display_help(Opts),
     set_verbosity(Opts),
     set_declaration_level(Opts),
     get_ontology_dir(Opts, ODir),
@@ -46,6 +57,13 @@ parse_args(ExtraArgs, Opts, PosArgs) :-
     load_recognizers(Opts),
     load_data_from_dir(Opts).
 
+display_help(Opts) :-
+    member(help(true), Opts),
+    opts_spec(Spec),
+    opt_help(Spec, Help),
+    write(Help),
+    halt.
+display_help(Opts) :- member(help(false), Opts).
 
 get_ontology_dir(Opts, Path) :- member(ontology_dir(Path), Opts), !.
 get_ontology_dir(_, '.').

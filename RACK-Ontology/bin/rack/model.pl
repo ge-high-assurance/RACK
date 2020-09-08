bc7815ad81529d21c74c113eabbd65dcb46a354e
Kevin Quick <kquick@galois.com>
2020-09-07 17:08:07 -0700

    Set uniqueIdentifier for all ingested data elements.

--- RACK-Ontology/bin/rack/model.pl
+++ RACK-Ontology/bin/rack/model.pl
@@ -653,6 +653,8 @@ data_get('PROV-S#ACTIVITY', 'PROV-S#startedAtTime', model_start(ModelName, Start
 data_get('PROV-S#ACTIVITY', 'PROV-S#endedAtTime', model_end(ModelName, EndTime), EndTime) :-
     rack_model_name(ModelName).
 
+data_get(_, 'PROV-S#uniqueIdentifier', uid(UID), UIDStr) :- atom_string(UID, UIDStr).
+
 data_get('SOFTWARE#FILE', 'SOFTWARE#filename', sw_file_data(_, Dir, NameOrPath), Value) :-
     file_to_fpath(NameOrPath, Dir, Path),
     atom_string(Path, Value).

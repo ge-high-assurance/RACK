a42278840a5b8db1d81ac0454ad34483c51acfc2
Kevin Quick <kquick@galois.com>
2020-09-09 00:22:15 -0700

    Move make step association to common databin rackfuncs.sh

--- RACK-Ontology/databin/rackfuncs.sh
+++ RACK-Ontology/databin/rackfuncs.sh
@@ -21,3 +21,21 @@ top_rel_curdir() {
     here=$(pwd | sed -e "s,^${tdir}/\?\(.*\),\1,")
     if [ -z $here ] ; then echo . ; else echo $here; fi
 }
+
+
+add_make_step() {
+    export IFS="@"
+    set ${1}
+    make_rackfile=$1
+    make_nonce=$2
+    echo "build_step(${make_nonce@Q}, ${nonce@Q})." >> ${make_rackfile}
+}
+
+update_make_steps() {
+(
+    export IFS=":"
+    for to_make in ${MAKE_DATABIN:-""} ; do
+        add_make_step $to_make
+    done
+)
+}

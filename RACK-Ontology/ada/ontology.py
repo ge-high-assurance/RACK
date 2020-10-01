fd5595779b3cb51703b3ddbbfac8700189e2beec
Eric Mertens <emertens@galois.com>
2020-10-01 11:15:36 -0700

    Elaborate the example

--- RACK-Ontology/ada/ontology.py
+++ RACK-Ontology/ada/ontology.py
@@ -4,21 +4,47 @@ from enum import Enum
 from rdflib import Namespace, Graph, URIRef, Literal
 
 NS = Namespace("http://arcos.rack/SOFTWARE#")
+RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+
+class Thing:
+    def __init__(self, uri):
+        self.node = URIRef(uri)
+        self.rdf_type = None
+    
+    def add_to_graph(self, graph):
+        graph.bind("sw", NS)
+        graph.add((self.node, RDF.type, self.rdf_type))
+
+class FileFormat(Thing):
+    def __init__(self, uri):
+        super().__init__(uri)
+        self.rdf_type = NS.FORMAT
+
+class File(Thing):
+    """File entity"""
+
+    def __init__(self, uri, filename, file_format):
+        super().__init__(uri)
+        self.rdf_type = NS.FILE
+        self.filename = filename
+        self.file_format = file_format
+
+    def add_to_graph(self, graph):
+        super().add_to_graph(graph)
+        graph.add((self.node, NS.filename, Literal(self.filename)))
+        graph.add((self.node, NS.fileFormat, Literal(self.file_format.node)))
 
 class ComponentType(Enum):
     """Software component types"""
     SOURCE_FUNCTION = "SOURCE_FUNCTION"
     MODULE = "MODULE"
 
-class Thing:
-    def __init__(self, uri):
-        self.node = URIRef(uri)
-
 class Component(Thing):
     """Software components"""
 
     def __init__(self, uri, name, ty: ComponentType):
         super().__init__(uri)
+        self.rdf_type = NS.COMPONENT
         self.name = name
         self.component_type = ty
         self.defined_in = None
@@ -33,9 +59,9 @@ class Component(Thing):
 
     def add_to_graph(self, graph: Graph):
         """Serialize the component into an RDF graph"""
-        graph.bind("sw", NS)
+        super().add_to_graph(graph)
 
-        graph.add((self.node, NS.COMPONENT_TYPE, NS[self.component_type]))
+        graph.add((self.node, NS.COMPONENT_TYPE, NS[self.component_type.value]))
         graph.add((self.node, NS.name, Literal(self.name)))
 
         if self.defined_in is not None:
--- RACK-Ontology/ada/rack_rdf.py
+++ RACK-Ontology/ada/rack_rdf.py
@@ -1,20 +1,28 @@
 """Demonstration driver"""
 import sys
-from rdflib import Graph, URIRef
-from ontology import Thing, Component, ComponentType
+from rdflib import Graph, Namespace
+from ontology import Component, ComponentType, File, FileFormat
 
 g = Graph()
-g.bind("dat", URIRef("http://data/"))
+FORMAT = Namespace("http://data/format#")
+DATA = Namespace("http://data/")
 
-my_file = Thing("http://data/src.txt")
+g.bind("dat", DATA)
+g.bind("format", FORMAT)
 
-c1 = Component("http://data/fun1", "fun1", ComponentType.SOURCE_FUNCTION)
+txt_format = FileFormat(FORMAT.TEXT_FILE)
+
+my_file = File(DATA['src.txt'], "src.txt", txt_format)
+
+c1 = Component(DATA['fun1'], "fun1", ComponentType.SOURCE_FUNCTION)
 c1.defined_in = my_file
 
-c2 = Component("http://data/fun2", "fun2", ComponentType.SOURCE_FUNCTION)
+c2 = Component(DATA['fun2'], "fun2", ComponentType.SOURCE_FUNCTION)
 c2.defined_in = my_file
 c2.add_mention(c1)
 
+txt_format.add_to_graph(g)
+my_file.add_to_graph(g)
 c1.add_to_graph(g)
 c2.add_to_graph(g)
 

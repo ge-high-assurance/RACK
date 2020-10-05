386105714477be56d003a9c9cf2d42c2ce74eba8
Valentin Robert <valentin.robert.42@gmail.com>
2020-10-05 20:48:01 +0200

    ada: add type stubs and some Python typings

--- RACK-Ontology/ada/shell.nix
+++ RACK-Ontology/ada/shell.nix
@@ -5,17 +5,25 @@
 let
   gnat = nixpkgs.gnat10;
   libadalang = import ./nix/libadalang.nix { inherit gnat nixpkgs pythonPackages; };
+  stubs = import ./stubs { inherit nixpkgs; };
 in
 nixpkgs.mkShell {
   buildInputs = with nixpkgs; [
     libadalang
+    pythonPackages.colorama
+    pythonPackages.mypy
     pythonPackages.pylint
+    stubs
   ];
 
+  # for quick access to this offline documentation, if needed
   AST_HTML="${libadalang}/share/libadalang/ast-types.html";
-  LIBADALANG_ADA_FILES="${libadalang}/include/libadalang";
-
+  # necessary so that the linker finds the libadalang files
   LD_LIBRARY_PATH="${libadalang}/lib";
+  # quick access to some Ada files, for testing purposes
+  LIBADALANG_ADA_FILES="${libadalang}/include/libadalang";
+  # variable used by mypy to find type signature stubs for libraries missing them
+  MYPYPATH="${stubs}";
 
   shellHook = ''
     export PATH=${libadalang}/bin:$PATH

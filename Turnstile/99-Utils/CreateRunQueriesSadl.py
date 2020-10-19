7dd31f4ace9dd2e7f5302a1a6d798d5f9838563a
Dan Russell <daniel.russell@ge.com>
2020-10-19 16:26:27 -0400

    Adding Generated CSV files for turnstile specific classes

--- Turnstile/99-Utils/CreateRunQueriesSadl.py
+++ Turnstile/99-Utils/CreateRunQueriesSadl.py
@@ -5,15 +5,9 @@ if __name__ == "__main__":
         for l in QueryStrings.readlines():
             if l.startswith('"zzzWrite'):
                 queries.append(l.replace('"zzz',"").replace('zzz"',"").replace('yyy','"'))
-    with open("PropertyDefinitionInference.sadl","r") as propertyInferenceSadl:
-        for l in propertyInferenceSadl.readlines():
-            if l.startswith("import") and l.find("rdf-schema")==-1:
-                imports.append(l)
     with open("RunQueryString.sadl", "w") as Outfile:
         Outfile.write('uri "http://Turnstile/RunQueries".\n\n')
-        for l in imports:
-            Outfile.write(l)
-        Outfile.write("\n")
+        Outfile.write('import "http://Turnstile/All".\n\n')
         for l in queries:
             Outfile.write(l)
     print("Done")

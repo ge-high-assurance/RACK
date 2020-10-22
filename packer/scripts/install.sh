2ea3b734af3d23e12c745b8bb3ea2d3ab156464c
Kevin Quick <kquick@galois.com>
2020-10-22 13:45:40 -0700

    Adds RACK ontology assist tools and SWI Prolog dependency to the distributions.

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -24,8 +24,9 @@ if [ "${PACKER_BUILDER_TYPE}" == "docker" ]; then
     export DEBIAN_FRONTEND=noninteractive
     export DEBCONF_NONINTERACTIVE_SEEN=true
 
+    add-apt-repository ppa:swi-prolog/stable
     apt-get update -y
-    apt-get install -y curl default-jre gettext-base nginx-light python3 unzip
+    apt-get install -y curl default-jre gettext-base nginx-light python3 unzip swi-prolog
 
     # Install docker-systemctl-replaement
 

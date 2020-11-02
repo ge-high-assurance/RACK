b58a23cbbc94d43f647d2bafa9fed58a311d6991
Dan <daniel.russell@ge.com>
2020-11-02 11:01:37 -0500

    commenting out the loading of turnstile data

--- cli/setup-rack.sh
+++ cli/setup-rack.sh
@@ -12,5 +12,5 @@ rack model import --clear ../RACK-Ontology/OwlModels/import.yaml
 rack nodegroups delete --yes --regexp --ignore-nonexistent "^ingest[0-9]+" "^query "
 rack nodegroups import ../nodegroups/ingestion
 rack nodegroups import ../nodegroups/queries
-rack data import --clear ../RACK-Ontology/models/TurnstileSystem/Data/import.yaml
-rack data import ../RACK-Ontology/OwlModels/requirements.yaml
+#rack data import --clear ../RACK-Ontology/models/TurnstileSystem/Data/import.yaml
+#rack data import ../RACK-Ontology/OwlModels/requirements.yaml

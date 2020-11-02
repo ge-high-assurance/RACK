af4b04d64a8d02e10fca523e3986b002c7b5619c
Eric Mertens <emertens@galois.com>
2020-11-02 14:41:15 -0800

    Remove hasAttribute

--- assist/bin/ontology/prov-s.pl
+++ assist/bin/ontology/prov-s.pl
@@ -4,15 +4,10 @@
           [
             activity/1,
             agent/1,
-            attribute/1,
             collection/1,
             entity/1,
-            field/1,
             thing/1,
             dataInsertedBy/2,
-            field/2,
-            fieldValue/2,
-            hasAttribute/2,
             startedAtTime/2,
             identifier/2,
             wasAssociatedWith/2,
@@ -26,16 +21,11 @@
 
 activity(C) :- rack_instance('PROV-S#ACTIVITY', C).
 agent(C) :- rack_instance('PROV-S#AGENT', C).
-attribute(C) :- rack_instance('PROV-S#ATTRIBUTE', C).
 collection(C) :- rack_instance('PROV-S#COLLECTION', C).
 entity(C) :- rack_instance('PROV-S#ENTITY', C).
-field(C) :- rack_instance('PROV-S#FIELD', C).
 thing(C) :- rack_instance('PROV-S#THING', C).
 
 dataInsertedBy(A, B) :- rdf(A, rack:'PROV-S#dataInsertedBy', B).
-field(A, B) :- rdf(A, rack:'PROV-S#field', B).
-fieldValue(A, B) :- rdf(A, rack:'PROV-S#fieldValue', B).
-hasAttribute(A, B) :- rdf(A, rack:'PROV-S#hasAttribute', B).
 startedAtTime(A, B) :- rdf(A, rack:'PROV-S#startedAtTime', B).
 identifier(A, B) :- rdf(A, rack:'PROV-S#identifier', B).
 wasAssociatedWith(A, B) :- rdf(A, rack:'PROV-S#wasAssociatedWith', B).

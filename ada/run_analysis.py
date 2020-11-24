c475c38982e3d4e5eaf0901388134e1dbf4278a8
Valentin Robert <valentin.robert.42@gmail.com>
2020-11-24 11:29:21 +0100

    ada: allow a file arg for listing project files
    Since you can only put so many files on the command line, it is useful
    to also allow a file to pass in a large amount of files.  One file per
    line, empty lines allowed.

--- ada/run_analysis.py
+++ ada/run_analysis.py
@@ -17,15 +17,32 @@ import static_call_graph as SCG
 # contains a default implementation for projects that have a GPR project file.
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--project", "-P", help="Project file (.gpr)", type=str)
-parser.add_argument("--file", "-f", help="File to analyze (.ada, .adb, .ads)", type=str, required=True)
-parser.add_argument("others", help="Other files of the project", type=str, nargs="+")
+parser.add_argument("--gpr",     help="Project file (.gpr)", type=str)
+parser.add_argument("--files",   help="Text file containing a list of project files, one per line", type=str)
+parser.add_argument("--analyze", help="File to analyze (.ada, .adb, .ads)", type=str, required=True)
+parser.add_argument("others",    help="List of project files (use --project-files if there are many)", type=str, nargs="*")
 args = parser.parse_args()
 
+# Default provider just looks through files listed as "others"
+provider = lal.UnitProvider.auto(input_files=args.others)
+
+def input_files_from_files_list(files_list: str) -> [str]:
+    """Returns the list of all non-empty lines from a given file."""
+    with open(files_list) as f:
+        lines = list(filter(None, f.read().splitlines()))
+        return lines
+
+# If a gpr project file is passed, it is used.
+# If not, and a file is passed that lists project files, it is used.
+# Otherwise, we just use the files listed on the remainder of the command line.
 provider = (
-    lal.UnitProvider.for_project(args.project)
-    if args.project
-    else lal.UnitProvider.auto(input_files=args.others)
+    lal.UnitProvider.for_project(args.gpr)
+    if args.gpr
+    else
+    lal.UnitProvider.auto(input_files=input_files_from_files_list(args.files))
+    if args.files
+    else
+    lal.UnitProvider.auto(input_files=args.others)
 )
 
 context = lal.AnalysisContext(unit_provider=provider)
@@ -49,7 +66,7 @@ def register_component(components, component: SCG.GraphNode) -> None:
     # TODO: check what we know about the actual component type
     components[key] = Component(DATA[uri], name, ComponentType.SOURCE_FUNCTION)
 
-file_to_analyze = args.file
+file_to_analyze = args.analyze
 
 unit = context.get_from_file(file_to_analyze)
 if unit.root:

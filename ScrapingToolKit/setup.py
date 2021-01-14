775be67fb73ddc8d807fb39183e23937b15349f3
daniel.russell <daniel.russell@ge.com>
2021-01-14 11:38:50 -0500

    adding setup.py

---
+++ ScrapingToolKit/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2021, General Electric Company, Inc.
+#
+# All Rights Reserved
+#
+# This material is based upon work supported by the Defense Advanced Research
+# Projects Agency (DARPA) under Contract No. FA8750-20-C-0203.
+#
+# Any opinions, findings and conclusions or recommendations expressed in this
+# material are those of the author(s) and do not necessarily reflect the views
+# of the Defense Advanced Research Projects Agency (DARPA).
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ScrapingToolKit",
+    packages=find_packages(),
+)

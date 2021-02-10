658f14bbeef0d8e0cdd5f6ace777834663e616e1
Eric Mertens <emertens@galois.com>
2021-02-10 11:21:24 -0800

    enhanced setup-rack.sh missing rack error message

--- cli/setup-rack.sh
+++ cli/setup-rack.sh
@@ -4,8 +4,20 @@ set -eu
 
 if ! command -v rack > /dev/null
 then
-    echo "rack cli tool not found in PATH"
-    exit 1
+	cat <<-END
+		ERROR: rack cli tool not found in PATH
+		
+		Installation instructions are available at
+		https://github.com/ge-high-assurance/RACK/wiki/RACK-CLI#install-dependencies
+		or locally in README.md
+		
+		If you've already installed RACK CLI, please activate your virtual environment
+		
+		macOS/Linux: source venv/bin/activate
+		Windows:     venv\\Scripts\\activate.bat
+		PowerShell:  venv\\Scripts\\Activate.ps1
+	END
+	exit 1
 fi
 
 rack model import --clear ../RACK-Ontology/OwlModels/import.yaml

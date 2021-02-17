eaed37caebebcf3598f08cf27c163bf0a0521e8c
Eric Mertens <emertens@galois.com>
2021-02-17 10:31:10 -0800

    Fixup the CLI tests

--- cli/tests/conftest.py
+++ cli/tests/conftest.py
@@ -4,17 +4,16 @@
 import pytest
 
 from time import sleep
+from typing import Any
 
 from semtk3 import check_services, set_host
 
 @pytest.fixture(scope="session")
-def rack_in_a_box(docker_ip: str, docker_services: str) -> str:
+def rack_in_a_box(docker_ip: str, docker_services: Any) -> str:
     """Ensure that RACK-in-a-box is up and responsive."""
     url = "http://{}".format(docker_ip)
     set_host(url)
-    # TODO(lb): For some reason, check_services always returns false.
-    # docker_services.wait_until_responsive(
-    #     timeout=240.0, pause=0.1, check=lambda: check_services()
-    # )
-    sleep(120)
+    docker_services.wait_until_responsive(
+         timeout=240.0, pause=0.1, check=lambda: check_services()
+    )
     return url

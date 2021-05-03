b8017ecbeaa69fa6185cd88e5b06360569c8d20d
Valentin Robert <valentin.robert.42@gmail.com>
2021-05-03 15:49:34 -0700

    migration: make the output Markdown-ready (#363)
    This makes it so that we can directly copy and paste the output of the
    `rack_migrate --list-changes-only --format-for-wiki` command into
    the wiki.

---
+++ migration/rack/commits/commit44da44c6877c881240c418d084ecb17de9443373.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2021, Galois, Inc.
+#
+# All Rights Reserved
+#
+# This material is based upon work supported by the Defense Advanced Research
+# Projects Agency (DARPA) under Contract No. FA8750-20-C-0203.
+#
+# Any opinions, findings and conclusions or recommendations expressed in this
+# material are those of the author(s) and do not necessarily reflect the views
+# of the Defense Advanced Research Projects Agency (DARPA).
+
+from ontology_changes import Commit
+
+commit = Commit(
+    number="44da44c6877c881240c418d084ecb17de9443373",
+    tag="v5.0",
+    changes=[
+        # no ontology change, just here for the tag
+    ],
+)

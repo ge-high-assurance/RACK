620d70c39211bc5d53975651ddb7b35bd2e2971e
Valentin Robert <valentin.robert.42@gmail.com>
2021-05-07 15:26:37 -0700

    migration: add v6.0 tag (#380)

---
+++ migration/rack/commits/commit76de25ee930683871febc1b4cc1e4386aca16d42.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2021, Galois, Inc.
+#
+# All Rights Reserved
+#
+# This material is based upon work supported by the Defense Advanced Research
+# Projects Agency (DARPA) under Contract No. FA8750-20-C-0203.
+#
+# Any opinions, findings and conclusions or recommendations expressed in this
+# material are those of the author(s) and do not necessarily reflect the views
+# of the Defense Advanced Research Projects Agency (DARPA).
+
+from ontology_changes import Commit
+
+commit = Commit(
+    number="76de25ee930683871febc1b4cc1e4386aca16d42",
+    tag="v6.0",
+    changes=[
+        # no ontology change, just here for the tag
+    ],
+)

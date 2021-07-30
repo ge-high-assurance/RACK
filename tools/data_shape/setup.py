ac54706bbebf1dbcb1467c496fbaefdbc8edf995
Valentin Robert <val@galois.com>
2021-07-30 11:14:00 -0700

    data_shape: use setup.cfg (#521)
    I had trouble setting up the environment with the given setup.py.  I went to
    read the documentation for setuptools, found out they recommended using
    setup.cfg nowadays, switched the configuration for testing purposes, and it
    worked.
    
    It's very similar but makes the configuration a proper data file rather than
    some constants in a piece of code.  If everyone is happy switching to
    that, here is how we could go about it.

--- tools/data_shape/setup.py
+++ tools/data_shape/setup.py
@@ -2,17 +2,5 @@
 
 from setuptools import setup
 
-setup(name='data_shape',
-      version='0.1',
-      description='RACK data shape',
-      author='Eric Mertens',
-      author_email='emertens@galois.com',
-      packages=['data_shape'],
-      scripts=[
-            'bin/data_shape',
-            'bin/collaboration'],
-      install_requires=[
-            'SPARQLWrapper',
-            'graphviz'
-      ],
-      zip_safe=False)
+if __name__ == "__main__":
+    setup()

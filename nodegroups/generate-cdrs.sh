5e5c22a2ba96a07dece9d2cc8c858d62efe4416a
John Interrante <interran@research.ge.com>
2021-11-30 22:09:52 -0500

    Generate new CDR files
    Develop generate-cdrs.sh script to run SemTK's IngestTemplateGenerator
    on RACK.  Run generate-cdrs.sh with `RACK/nodegroups/generate-cdrs.sh
    semtk-opensource/standaloneExecutables/target/standaloneExecutables-jar-with-dependencies.jar`,
    see first run fail with exception "No enum constant ANYURI", fix by
    generating new OWL files to pick up change to PROV-S.sadl (string
    instead of anyURI), and see second run succeed now.  Also add step to
    GitHub Actions workflows to start generating CDR files automatically.
    
    action.yml: Add step to generate CDR files automatically in GitHub
    Actions jobs.
    
    **/SadlImplicitModel.owl: Changes due to indeterminate ordering, not
    worth paying attention to.
    
    PROV-S.owl: Pick up change to PROV-S.sadl (string instead of anyURI).
    
    nodegroups/CDR/*.csv: Regenerate using generate-cdrs.sh with changes
    to many files which will need to be tested.
    
    nodegroups/generate-cdrs.sh: Develop script to clear rack model,
    import RACK models, remove old CDR files, and generate new CDR files.
    
    nodegroups/ingestion/**.json: Regenerate using generate-cdrs.sh with
    changes to many files which will need to be tested.
    
    nodegroups/ingestion/**.csv: Regenerate using generate-cdrs.sh with
    changes to a few files which will need to be tested.

---
+++ nodegroups/generate-cdrs.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Copyright (c) 2021, General Electric Company and Galois, Inc.
+set -eu
+BASEDIR=$(dirname "$0")
+
+# You need to pass in the standalone executables jar
+
+STANDALONE_EXE_JAR=$(realpath "$1")
+
+# Check rack cli tool is in PATH
+
+cd "$BASEDIR"/..
+cli/ensure-cli-in-PATH.sh
+
+# Import ontologies
+
+rack model clear
+rack model import RACK-Ontology/OwlModels/import.yaml
+rack model import GE-Ontology/OwlModels/import.yaml
+rack model import Boeing-Ontology/OwlModels/import.yaml
+rack model import GrammaTech-Ontology/OwlModels/import.yaml
+rack model import LM-Ontology/OwlModels/import.yaml
+rack model import SRI-Ontology/OwlModels/import.yaml
+rack model import STR-Ontology/OwlModels/import.yaml
+
+# Remove old CDR files
+
+rm -rf nodegroups/CDR 
+rm -rf nodegroups/ingestion
+
+# Generate new CDR files
+
+java -cp "$STANDALONE_EXE_JAR" com.ge.research.semtk.standaloneExecutables.IngestTemplateGenerator --csv-dir nodegroups/CDR --json-dir nodegroups/ingestion

28d1662c95f87c90a53d07f2312db8624ecc66ce
Eric Mertens <emertens@galois.com>
2021-12-07 15:57:48 -0800

    Support copying and building with the setup-owl script

--- cli/setup-build-owl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copyright (c) 2021, General Electric Company and Galois, Inc.
-
-set -e
-
-RACK_DIR=$(realpath "$(dirname "$0")"/..)
-
-docker run --rm -u 0 -e RUN_AS="$(id -u) $(id -g)" -v "$RACK_DIR:/RACK" sadl/sadl-eclipse:v3.5.0-20211204 -importAll /RACK -cleanBuild

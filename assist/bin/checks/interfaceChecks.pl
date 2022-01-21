4c63457b7e86f98adef909c0edf4426c14506e7c
Kevin Quick <kquick@galois.com>
2022-01-21 08:52:30 -0800

    [ASSIST-DV] enhance INTERFACE checks for missing source SYSTEMs.

--- assist/bin/checks/interfaceChecks.pl
+++ assist/bin/checks/interfaceChecks.pl
@@ -20,8 +20,10 @@ something_is_true.
 
 :- use_module(rack(model)).
 
-prolog:message(no_dest_system(IFace, Name)) -->
+prolog:message(missing_dest_system(IFace, Name)) -->
     [ 'INTERFACE ~w (~w) has no destination SYSTEM'-[IFace, Name] ].
+prolog:message(missing_src_system(IFace, Name)) -->
+    [ 'INTERFACE ~w (~w) has no source SYSTEM'-[IFace, Name] ].
 
 get_interface_instance(IFACE) :-
     rdf_reachable(C, rdf:subClass, 'http://arcos.rack/SYSTEM#INTERFACE'),
@@ -31,9 +33,13 @@ interface_ident(IFACE, Name) :-
     rdf(IFACE, 'http://arcos.rack/PROV-S#identifier', Name), !.
 interface_ident(_, "<unknown>").
 
-no_system(IFACE) :-
+no_dest_system(IFACE) :-
     rdf(IFACE, 'http://arcos.rack/SYSTEM#destination', _), !, fail.
-no_system(_).
+no_dest_system(_).
+
+no_src_system(IFACE) :-
+    rdf(IFACE, 'http://arcos.rack/SYSTEM#source', _), !, fail.
+no_src_system(_).
 
 %! check_INTERFACE_no_dest_SYSTEM is det.
 %
@@ -41,12 +47,23 @@ no_system(_).
 %    Always succeeds, emits warnings.
 check_INTERFACE_no_dest_SYSTEM(IFACE) :-
     get_interface_instance(IFACE),
-    no_system(IFACE),
+    no_dest_system(IFACE),
     interface_ident(IFACE, Name),
-    print_message(warning, no_dest_system(IFACE, Name)).
+    print_message(warning, missing_dest_system(IFACE, Name)).
+
+%! check_INTERFACE_no_src_SYSTEM is det.
+%
+%    Checks that no INTERFACE is lacking a source SYSTEM.
+%    Always succeeds, emits warnings.
+check_INTERFACE_no_src_SYSTEM(IFACE) :-
+    get_interface_instance(IFACE),
+    no_src_system(IFACE),
+    interface_ident(IFACE, Name),
+    print_message(warning, missing_src_system(IFACE, Name)).
 
 %! check_INTERFACE is det.
 %
 %    Performs all checks for INTERFACEs.  Always succeeds, emits warnings.
 check_INTERFACE(IFACE) :-
-    check_INTERFACE_no_dest_SYSTEM(IFACE).
+    check_INTERFACE_no_dest_SYSTEM(IFACE);
+    check_INTERFACE_no_src_SYSTEM(IFACE).

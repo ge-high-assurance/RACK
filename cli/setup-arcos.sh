f066387bb99ad7dfd57e5b7b47d8138f67549753
Eric Mertens <emertens@galois.com>
2022-04-07 08:32:51 -0700

    Add RTX overlay to support synthetic analysis outputs (#689)
    Add RTX-Ontology for synthetic analysis outputs
    
    Co-authored-by: Valentin Robert <val@galois.com>

--- cli/setup-arcos.sh
+++ cli/setup-arcos.sh
@@ -24,6 +24,10 @@ rack nodegroups import ../nodegroups/ingestion/arcos.descert
 rack model import ../STR-Ontology/OwlModels/import.yaml
 rack nodegroups import ../nodegroups/ingestion/arcos.arbiter
 
+# RTX model & auto-generated nodegroups
+rack model import ../RTX-Ontology/OwlModels/import.yaml
+rack nodegroups import ../nodegroups/ingestion/arcos.aace
+
 ### Applicable Standards ### copy any of these to the instance data load script and use when applicable
 # rack data import --clear ../RACK-Ontology/OwlModels/ARP-4754A.yaml     # from datagraph http://rack001/arp-4754a
 # rack data import --clear ../RACK-Ontology/OwlModels/DO-330.yaml        # from datagraph http://rack001/do-330

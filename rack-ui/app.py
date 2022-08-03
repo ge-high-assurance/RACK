ca278ad48f7451694fdf9cce3fe1672736bc649d
Jenny Williams <weisenje@research.ge.com>
2022-08-03 13:39:36 -0400

    Show status when resetting RACK

--- rack-ui/app.py
+++ rack-ui/app.py
@@ -10,8 +10,10 @@ from zipfile import ZipFile
 from datetime import datetime
 from pathlib import Path
 import glob
+from contextlib import redirect_stdout
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+f = io.StringIO()
 
 BASE_URL = "http://localhost"
 TRIPLE_STORE = "http://localhost:3030/RACK"
@@ -94,6 +96,7 @@ modals = html.Div(
                 dbc.ModalFooter(dbc.Button("Close", id="button-modal-reset-close", className="ms-auto", n_clicks=0)),
             ],
             id="modal-reset",
+            size="lg",
             is_open=False,
             backdrop=False,
         ),
@@ -103,6 +106,13 @@ modals = html.Div(
 content = html.Div(id="page-content", style=CONTENT_STYLE)
 
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content, modals])
+
+@app.callback(Output("div-status", "children"),
+              Input("interval-component", "n_intervals"))
+def update_status(n):
+    """ Callback triggered at regular interval to update status """
+    return f.getvalue()
+
 
 @app.callback(Output("page-content", "children"), 
               Input("url", "pathname"))
@@ -173,34 +183,43 @@ def reset(n_clicks_reset, n_clicks_close, modal_is_open):
     """ Callback triggered when user selects reset """
     if not modal_is_open and n_clicks_reset is not None and n_clicks_reset > 0:
         try:
-            # clear all model/data/nodegroups
-            # TODO remove when manifest supports "clear-first"
-            rack.clear_driver(BASE_URL, ["http://rack001/model"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.MODEL)
-            rack.clear_driver(BASE_URL, ["http://rack001/data"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.DATA)
-            rack.clear_driver(BASE_URL, ["http://rack001/arp-4754a"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.DATA)
-            rack.clear_driver(BASE_URL, ["http://rack001/do-330"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.DATA)
-            rack.clear_driver(BASE_URL, ["http://rack001/do-178c"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.DATA)
-            rack.clear_driver(BASE_URL, ["http://rack001/mitre-cwe"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.DATA)
-            rack.delete_all_nodegroups_driver(yes=True, base_url=BASE_URL)
+            with redirect_stdout(f):
 
-            # load RACK
-            rack.ingest_manifest_driver(Path("../cli/manifest-rack.yaml"), BASE_URL, TRIPLE_STORE, TRIPLE_STORE_TYPE, True)
+                # clear all model/data/nodegroups
+                # TODO remove when manifest supports "clear-first"
+                rack.clear_driver(BASE_URL, ["http://rack001/model"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.MODEL)
+                rack.clear_driver(BASE_URL, ["http://rack001/data"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.DATA)
+                rack.clear_driver(BASE_URL, ["http://rack001/arp-4754a"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.DATA)
+                rack.clear_driver(BASE_URL, ["http://rack001/do-330"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.DATA)
+                rack.clear_driver(BASE_URL, ["http://rack001/do-178c"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.DATA)
+                rack.clear_driver(BASE_URL, ["http://rack001/mitre-cwe"], TRIPLE_STORE, TRIPLE_STORE_TYPE, Graph.DATA)
+                rack.delete_all_nodegroups_driver(yes=True, base_url=BASE_URL)
+
+                # load RACK
+                rack.ingest_manifest_driver(Path("../cli/manifest-rack.yaml"), BASE_URL, TRIPLE_STORE, TRIPLE_STORE_TYPE, True)
+
             return True, "RACK has been reset"  # show modal dialog
         except Exception as e:
             return True, get_error_trace(e)  # show modal dialog with error
+    elif modal_is_open and n_clicks_close is not None and n_clicks_close > 0:  # user clicked close on modal dialog
+        f.truncate(0)  # clear status
+        f.seek(0)  # clear status
     return False, ""  # hide (or don't show) modal dialog
 
 
 def page_main() -> html.Div:
     """ Components for main page """
     try:
-        return html.Div(
+        return html.Div([
             html.Table([
                 html.Tr(dcc.Markdown("Welcome to RACK.")),
                 html.Tr(html.Button('Load ARCOS', id='button-load-arcos', style=BUTTON_STYLE)),
                 html.Tr(dcc.Upload( html.Button('Load ingestion package', style=BUTTON_STYLE), id='button-upload', accept=".zip", multiple=True)),
                 html.Tr(dcc.ConfirmDialogProvider(children=html.Button('Reset', style=BUTTON_STYLE), id='button-reset-provider', message='Are you sure you want to reset RACK?')),
-            ])
+            ]),
+            html.Div(id="div-status"),
+            dcc.Interval(id='interval-component', interval=1*1000, n_intervals=0)
+            ]
         )
     except Exception as e:
         return display_error(e)

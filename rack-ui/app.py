10cd8b1fc086f98de1def10831ddae8774f603ea
Jenny Williams <weisenje@research.ge.com>
2022-08-10 12:58:11 -0400

    Add link to launch SPARQLGraph with connection corresponding to manifest load.  Limit upload to 1 zip file.  Disable load button while loading.  Simplify code.

--- rack-ui/app.py
+++ rack-ui/app.py
@@ -4,6 +4,7 @@ import glob
 import traceback
 import io
 import base64
+import urllib
 from dash import Input, Output, State, dcc, html, dash_table
 from zipfile import ZipFile
 from datetime import datetime
@@ -11,7 +12,7 @@ from pathlib import Path
 from contextlib import redirect_stdout
 
 import rack
-from rack import Graph
+from rack import Graph, Manifest
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 in_mem_file = io.StringIO()   # stores loading status
@@ -19,6 +20,9 @@ in_mem_file = io.StringIO()   # stores loading status
 BASE_URL = "http://localhost"
 TRIPLE_STORE = "http://localhost:3030/RACK"
 TRIPLE_STORE_TYPE = "fuseki"
+
+# name of default manifest file within ingestion package
+MANIFEST_FILE_NAME = "manifest.yaml"
 
 # style for sidebar
 SIDEBAR_STYLE = {
@@ -68,14 +72,15 @@ sidebar = html.Div(
 )
 
 # modal dialogs
-modals = html.Div(
+dialogs = html.Div(
     [
+        # dialog confirming load done (or showing error)
         dbc.Modal(
             [
-                dbc.ModalBody("MESSAGE", id="div-modal-upload"),
-                dbc.ModalFooter(dbc.Button("Close", id="button-modal-upload-close", className="ms-auto", n_clicks=0)),
+                dbc.ModalBody("MESSAGE", id="load-done-dialog-div"),
+                dbc.ModalFooter(dbc.Button("Close", id="load-done-dialog-button", className="ms-auto", n_clicks=0)),
             ],
-            id="modal-upload",
+            id="load-done-dialog",
             is_open=False,
             backdrop=False,
         ),
@@ -84,7 +89,7 @@ modals = html.Div(
 
 content = html.Div(id="page-content", style=CONTENT_STYLE)
 
-app.layout = html.Div([dcc.Location(id="url"), sidebar, content, modals])
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content, dialogs])
 
 @app.callback(Output("page-content", "children"), 
               Input("url", "pathname"))
@@ -100,45 +105,62 @@ def render_page_content(pathname: str) -> dbc.Container:
         ]
     )
 
+@app.callback(Output('load-button', 'disabled'),
+              Input('load-button', 'contents'),
+              Input('load-done-dialog-button', 'n_clicks'),
+              State('load-done-dialog', 'is_open'),
+              prevent_initial_call=True)
+def disable_upload_button(zip_file_contents, n_clicks_close, load_done_dialog_is_open):
+    """ Callback to disable the load button while a load is in progress """
+    if not load_done_dialog_is_open and zip_file_contents is not None:  # user clicked the load button
+        return True  # disable the upload button
+    return False  # enable the upload button
+
 @app.callback(Output("div-status", "children"),
               Input("interval-component", "n_intervals"))
 def update_status(n):
     """ Callback triggered at regular interval to update loading status """
     return in_mem_file.getvalue()
 
-@app.callback([Output('modal-upload', 'is_open'), Output('div-modal-upload', 'children')],
-              Input('button-upload', 'contents'),
-              Input('button-upload', 'filename'),
-              Input('button-upload', 'last_modified'),
-              Input('button-modal-upload-close', 'n_clicks'),
-              State('modal-upload', 'is_open'),
+@app.callback(Output('load-done-dialog', 'is_open'),
+              Output('load-done-dialog-div', 'children'),
+              Input('load-button', 'contents'),
+              Input('load-done-dialog-button', 'n_clicks'),
+              State('load-done-dialog', 'is_open'),
               prevent_initial_call=True)
-def upload_ingestion_package(list_of_contents, list_of_names, list_of_dates, n_clicks_close, modal_is_open):
+def upload_ingestion_package(zip_file_contents, n_clicks_close, load_done_dialog_is_open):
     """ Callback triggered when user selects an ingestion package to load """
-    if not modal_is_open and list_of_contents is not None:  # user clicked the load button
+    if not load_done_dialog_is_open and zip_file_contents is not None:  # user clicked the load button
         try:
             with redirect_stdout(in_mem_file):  # capture the load status for display
-                for content, name, date in zip(list_of_contents, list_of_names, list_of_dates):
-                    tmp_dir = "/tmp/ingestion_package_uploaded_" + datetime.now().strftime("%Y%m%d-%H%M%S")  # temp directory to store the unzipped package
-                    content_type, content_string = content.split(',')
-                    zip_str = io.BytesIO(base64.b64decode(content_string))
-                    zip_obj = ZipFile(zip_str, 'r')
-                    zip_obj.extractall(path=tmp_dir)  # unzip the package
-                    default_manifest_file_name = "manifest.yaml"  # look for this manifest file
-                    manifests = glob.glob(tmp_dir + '/**/' + default_manifest_file_name, recursive=True)
-                    if len(manifests) == 0:
-                        raise Exception("Cannot load ingestion package: does not contain a default manifest file (" + default_manifest_file_name + ")")
-                    if len(manifests) > 1:
-                        raise Exception("Cannot load ingestion package: contains multiple default manifest files: " + str(manifests))
-                    manifest = manifests[0]
-                    rack.ingest_manifest_driver(Path(manifest), BASE_URL, TRIPLE_STORE, TRIPLE_STORE_TYPE, True)  # process the manifest
-            return True, "Loaded ingestion package using manifest file " + manifest[(len(tmp_dir) + 1):]  # show modal dialog with success message
+                tmp_dir = "/tmp/ingestion_package_uploaded_" + datetime.now().strftime("%Y%m%d-%H%M%S")  # temp directory to store the unzipped package
+                zip_str = io.BytesIO(base64.b64decode(zip_file_contents.split(',')[1]))
+                zip_obj = ZipFile(zip_str, 'r')
+                zip_obj.extractall(path=tmp_dir)  # unzip the package
+                manifest_paths = glob.glob(tmp_dir + '/**/' + MANIFEST_FILE_NAME, recursive=True)
+                if len(manifest_paths) == 0:
+                    raise Exception("Cannot load ingestion package: does not contain manifest file " + MANIFEST_FILE_NAME)
+                if len(manifest_paths) > 1:
+                    raise Exception("Cannot load ingestion package: contains multiple default manifest files: " + str(manifests))
+                manifest_path = manifest_paths[0]
+
+                # ingest the manifest
+                rack.ingest_manifest_driver(Path(manifest_path), BASE_URL, TRIPLE_STORE, TRIPLE_STORE_TYPE, True)  # process the manifest
+
+                # get connection from manifest, construct SPARQLGraph URL
+                with open(manifest_path, mode='r', encoding='utf-8-sig') as manifest_file:
+                    manifest = Manifest.fromYAML(manifest_file)
+                conn = manifest.getConnection()
+                sparqlgraph_url_str = "http://localhost:8080/sparqlGraph/main-oss/sparqlGraph.html?conn=" + urllib.parse.quote(conn, safe="")
+
+            return True, html.Div([dcc.Markdown("Loaded ingestion package."), html.A("Open in SPARQLGraph UI", href=sparqlgraph_url_str, target="_blank", style={"margin-top": "100px"}) ]
+        )
         except Exception as e:
-            return True, get_error_trace(e)  # show modal dialog with error
-    elif modal_is_open and n_clicks_close is not None and n_clicks_close > 0:  # user clicked close on modal dialog
+            return True, get_error_trace(e)  # show load done dialog with error
+    elif load_done_dialog_is_open and n_clicks_close is not None and n_clicks_close > 0:  # user clicked close on the load done dialog
         in_mem_file.truncate(0)  # clear status
         in_mem_file.seek(0)  # clear status
-    return False, ""  # hide (or don't show) modal dialog
+    return False, ""  # hide (or don't show) load done dialog
 
 def page_main() -> html.Div:
     """ Components for main page """
@@ -146,7 +168,7 @@ def page_main() -> html.Div:
         return html.Div([
             html.Table([
                 html.Tr(dcc.Markdown("Welcome to RACK.")),
-                html.Tr(dcc.Upload( html.Button('Load ingestion package', style=BUTTON_STYLE), id='button-upload', accept=".zip", multiple=True)),
+                html.Tr(dcc.Upload( html.Button('Load ingestion package', style=BUTTON_STYLE), id='load-button', accept=".zip", multiple=False)),
             ]),
             html.Div(id="div-status", style={"margin-top": "100px", "white-space": "pre-wrap", "border-style": "none", "height": 500, "width": 1200, "overflow-y": "auto", "display": "flex", "flex-direction": "column-reverse"}),
             dcc.Interval(id='interval-component', interval=1*1000, n_intervals=0)

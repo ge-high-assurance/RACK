624815bffcd45f041afc057f17ed91563f796e1b
russell-d-e <daniel.russell@ge.com>
2022-09-07 11:37:21 -0400

    updating entity resolution tool

---
+++ EntityResolution/TestData/Create-RACK-DATA.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+import os.path
+import shutil
+from Evidence import createEvidenceFile, createCDR
+import Evidence.Add as Add
+if __name__ == "__main__":
+    #################################################################
+    if os.path.exists(os.path.join(".","Package-1")):
+        shutil.rmtree(os.path.join(".","Package-1"))
+    createEvidenceFile(ingestionTitle="Package 1")
+    Add.TESTING.TEST(identifier="{L1-TST-1}", description="This is test L1-1.", verifies_identifier="{L1-REQ-1}")
+    Add.TESTING.TEST(identifier="{L1-TST-2}", description="This is test L1-2.", verifies_identifier="{L1-REQ-1}")
+    Add.REQUIREMENTS.REQUIREMENT(identifier="{L1-REQ-1}")
+    createCDR()
+    os.rename(os.path.join(".","RACK-DATA"), os.path.join(".","Package-1"))
+    
+    #################################################################                  
+    if os.path.exists(os.path.join(".","Package-2")):
+        shutil.rmtree(os.path.join(".","Package-2"))
+    createEvidenceFile(ingestionTitle="Package 2")
+    Add.REQUIREMENTS.REQUIREMENT(identifier="L1-REQ-1", description="This is requirement L1-1.", satisfies_identifier="L0-REQ-1")
+    Add.REQUIREMENTS.REQUIREMENT(identifier="L1-REQ-2", description="This is requirement L1-2.", satisfies_identifier="L0-REQ-1")
+    Add.REQUIREMENTS.REQUIREMENT(identifier="L0-REQ-1")
+    createCDR()
+    os.rename(os.path.join(".","RACK-DATA"), os.path.join(".","Package-2"))
+    
+    #################################################################    
+    if os.path.exists(os.path.join(".","Package-3")):
+        shutil.rmtree(os.path.join(".","Package-3"))
+    createEvidenceFile(ingestionTitle="Package 3")
+    Add.REQUIREMENTS.REQUIREMENT(identifier="[L1-REQ-2]",  satisfies_identifier="[L0-REQ-2]")
+    Add.REQUIREMENTS.REQUIREMENT(identifier="[L1-REQ-3]",  satisfies_identifier="[L0-REQ-2]")
+    Add.REQUIREMENTS.REQUIREMENT(identifier="[L0-REQ-2]")
+    createCDR()
+    os.rename(os.path.join(".","RACK-DATA"), os.path.join(".","Package-3"))
+    
+    #################################################################    
+    if os.path.exists(os.path.join(".","Resolutions-1")):
+        shutil.rmtree(os.path.join(".","Resolutions-1"))
+    createEvidenceFile(ingestionTitle="Resolutions-1")
+    Add.EntityResolution.AssumedSameAs(identifier="REQUIREMENTS:REQUIREMENT#L1-REQ-1",  primaryThing_identifier="L1-REQ-1", secondaryThing_identifier="{L1-REQ-1}")
+    createCDR()
+    os.rename(os.path.join(".","RACK-DATA"), os.path.join(".","Resolutions-1"))
+    
+    #################################################################    
+    if os.path.exists(os.path.join(".","Resolutions-2")):
+        shutil.rmtree(os.path.join(".","Resolutions-2"))
+    createEvidenceFile(ingestionTitle="Resolutions-2")
+    Add.EntityResolution.AssumedSameAs(identifier="REQUIREMENTS:REQUIREMENT#L1-REQ-2",  primaryThing_identifier="L1-REQ-2", secondaryThing_identifier="[L1-REQ-2]")
+    createCDR()
+    os.rename(os.path.join(".","RACK-DATA"), os.path.join(".","Resolutions-2"))
---
+++ EntityResolution/TestData/Load-Package2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Copyright (c) 2020, General Electric Company and Galois, Inc.
+set -eu
+BASEDIR=$(cd "$(dirname "$0")"; pwd)
+echo "$BASEDIR"
+if ! command -v rack > /dev/null
+then
+	cat <<-END
+		ERROR: rack cli tool not found in PATH
+		
+		Installation instructions are available at
+		https://github.com/ge-high-assurance/RACK/wiki/RACK-CLI#install-dependencies
+		or locally in README.md
+		
+		If you've already installed RACK CLI, please activate your virtual environment
+		
+		macOS/Linux: source venv/bin/activate
+		Windows:     venv\\Scripts\\activate.bat
+		PowerShell:  venv\\Scripts\\Activate.ps1
+	END
+	exit 1
+fi
+
+# suppress RACK cli warnings about missing columns
+export LOG_LEVEL=ERROR
+
+if test "$OSTYPE" == "cygwin" -o "$OSTYPE" == "msys"; then
+URLBASE=$(cygpath -m "$BASEDIR")
+else
+URLBASE="$BASEDIR"
+fi
+
+
+echo "Ingesting Package 2 ..."
+rack data import "$BASEDIR"/Package-2/import.yaml
+

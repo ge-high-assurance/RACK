0be6dd9908daeb062f69e868940cf13c79a4f6a5
russell-d-e <daniel.russell@ge.com>
2023-01-13 12:31:25 -0500

    fixed filtering check buttons and improved select class windows by adding sorting by column

--- EntityResolution/CheckBar.py
+++ EntityResolution/CheckBar.py
@@ -5,15 +5,16 @@ from tkinter import *
 class Checkbar(Frame):
     def __init__(self, parent=None, picks=[], side=LEFT, anchor=W,  command=None):
         Frame.__init__(self, parent)
+        self.command = command
         self.vars = {}
+        self.buttons = []
         for pick in picks:
-            var = IntVar()
-            chk = Checkbutton(self, text=pick, variable=var,  command=command)
-            chk.pack(side=side, anchor=anchor, expand=YES)
-            self.vars[pick]=var
-            self.vars[pick].set(1)
+            self.buttons.append(Checkbutton(self, text=pick, command=lambda pick=pick: self.callback(pick)))
+            self.buttons[-1].pack(side=side, anchor=anchor, expand=YES)
+            self.vars[pick] = True
+            self.buttons[-1].select()
+    def callback(self,  pick):
+        self.vars[pick] = not self.vars[pick]
+        self.command()
     def state(self):
-        r = {}
-        for v in self.vars:
-           r[v] = self.vars[v].get()
-        return r
+        return self.vars

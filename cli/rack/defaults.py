e19cef546e88f7e0ea6dd5460eecb9eafca5ba9f
Eric Mertens <emertens@galois.com>
2023-03-22 11:51:52 -0700

    Refactor default URLs into a separate module

---
+++ cli/rack/defaults.py
@@ -0,0 +1,9 @@
+from rack.types import Url
+
+DEFAULT_BASE_URL: Url = Url("http://localhost")
+DEFAULT_OPTIMIZE_URL: Url = Url("http://localhost:8050/optimize")
+
+MODEL_GRAPH: Url = Url("http://rack001/model")
+DEFAULT_DATA_GRAPH = Url("http://rack001/data")
+DEFAULT_TRIPLE_STORE = Url("http://localhost:3030/RACK")
+DEFAULT_TRIPLE_STORE_TYPE = "fuseki"

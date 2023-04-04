f36c42376871243419866f5e7a4b21bc7df9f28a
Kevin Quick <kquick@galois.com>
2023-04-04 14:29:07 -0700

    Generate Turnstile assist-dc owl files via Assemble-Turnstile.sh
    Replaces Load-Turnstile.sh.

---
+++ Turnstile-Example/Assemble-TurnstileData.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Copyright (c) 2020, General Electric Company and Galois, Inc.
+set -eu
+BASEDIR=$(cd "$(dirname "$0")"; pwd)
+echo "$BASEDIR"
+TOPDIR="${BASEDIR}/.."
+ASSISTDIR="${TOPDIR}/assist"
+
+cd "$BASEDIR"/Turnstile-IngestionPackage/CounterApplicationImplementation
+export PATH="${ASSISTDIR}/bin:${ASSISTDIR}/databin:${PATH}"
+make clean
+make
+make test
+make dist
+ingest_data -r "${BASEDIR}"/Turnstile-IngestionPackage/turnstile-ingest.rack \
+            -O "${TOPDIR}"/GE-Ontology \
+            -O "${TOPDIR}"/RACK-Ontology \
+            -o "${BASEDIR}"/turnstile-build.owl \
+            http://rack001/turnstiledata .

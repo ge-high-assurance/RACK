1160fbfd3b1369a6958ab1fce6d67659c6a030a3
Valentin Robert <val@galois.com>
2023-04-27 10:37:10 -0700

    add comment

--- ScrapingToolKit/setup.py
+++ ScrapingToolKit/setup.py
@@ -23,6 +23,11 @@ from AutoGeneration.GenerateSTK import autogen
 class CustomInstall(install):
     def run(self):
         autogen()
+        # This is a bit ugly, but unfortunately this `run` gets called after
+        # setuptools has already copied files from the source directory to the
+        # build directory.
+        # I could not find a way to hook prior to that copy being made, so this
+        # is a crutch to add those generated files a posteriori.
         shutil.copy("Evidence/Add.py", "build/lib/Evidence/Add.py")
         shutil.copy("Evidence/CONSTANTS.py", "build/lib/Evidence/CONSTANTS.py")
         shutil.copy("Evidence/RACK-DATA.xsd", "build/lib/Evidence/RACK-DATA.xsd")
